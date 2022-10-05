c1ca1bb57633854d44460421a7c32b4cfb61733e
Eunbi Yoon <yinyinbei0717@gmail.com>
2022-10-05 16:15:26 -0500

    adfadf

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ entries=[]
 
 @app.route("/", methods=["GET","POST"])
 def home():
+    print([e for e in app.db.entries.find({})])
     if request.method=="POST":
         entry_content=request.form.get("content")
         formatted_date=datetime.datetime.today().strftime("%Y-%m-%d")
         entries.append((entry_content, formatted_date))
-        app.db.entries.insert({"content": entry_content, "date": formatted_date})
 
     entries_with_date=[
         (
